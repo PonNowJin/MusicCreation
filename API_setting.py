60ee3afb96265ff00e5f433282a9ac21b7320f07
PonNowJin <pontzufu197@gmail.com>
2024-07-31 19:49:02 +0800

    main

--- API_setting.py
+++ API_setting.py
@@ -2,7 +2,9 @@ import google.generativeai as genai
 
 
 class LLM:
-    API_KEY = ''
+    API_KEY = 'AIzaSyCjbWCrwJTJHPUHvgfrNWFBifKoA_T6GVk'
+    # API_KEY = 'AIzaSyAoDS3qbAS2pH4EAKSRbogQm5bjbNaZqqw'
+    # API_KEY = 'AIzaSyBtXP_PXADcoS5F5-EctxnMV8yZuaOoBuY' 
     genai.configure(api_key=API_KEY)
 
     def __init__(self):
---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from Evaluation import Evaluation
+from Prompt import Prompt
+from google.generativeai.types.generation_types import StopCandidateException
+from Prompt_optimize import Prompt_OPT
+
+file = 'lyrics.txt'
+with open(file, 'w', encoding='utf-8') as f:
+    pass
+
+promptCreator_llm = Prompt()
+evaluation_llm = Evaluation()
+prompt_opt = Prompt_OPT()
+
+topic = """背影"""
+prompt_opt.setInputPrompt(topic)
+topic += prompt_opt.sendMsg()
+
+promptCreator_llm.setInputPrompt(topic)
+evaluation_llm.setTopic(topic)
+
+evaluation = None
+# 做第一次歌詞、曲風生成
+try:
+    promptCreator_llm.sendMsg()
+    evaluation = evaluation_llm.evaluation()  # evaluation為評分和建議的text
+except StopCandidateException as e:
+    print(f"安全政策異常:")
+"""
+except Exception:
+    print("出現錯誤")
+"""
+
+# 做prompt的變異
+while evaluation_llm.score < 90:
+    try:
+        promptCreator_llm.sendMsg(evaluation)
+        evaluation = evaluation_llm.evaluation()
+        print(evaluation)
+    except StopCandidateException as e:
+        print(f"安全政策異常:")
+        continue
+    except Exception as e:
+        print(f"出現錯誤")
+        continue
+
+
